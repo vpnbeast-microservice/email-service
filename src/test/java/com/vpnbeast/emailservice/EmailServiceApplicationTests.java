f7e0ba295ac7cd4cc30173ea776986abe6bb0252
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-21 23:23:28 +0300

    disable config server by annotation

--- src/test/java/com/vpnbeast/emailservice/EmailServiceApplicationTests.java
+++ src/test/java/com/vpnbeast/emailservice/EmailServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.vpnbeast.emailservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
 class EmailServiceApplicationTests {
 
     @Test
